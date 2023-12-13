d2a1ec61b8e40e630485eefdfdc23eb52892ebca
Rodrigo Azevedo <rodrigobivarazevedo@gmail.com>
2023-12-13 12:42:04 +0100

    started creating booking calendar page

--- db/test4.php
+++ db/test4.php
@@ -7,8 +7,8 @@ $pdo = new Appointmentsinfo();
 
 // Bind parameters
 $UserID = 1; // replace with the actual UserID
-$DoctorID = 2; // replace with the actual DoctorID
-$ClinicID = 2;
+$DoctorID = 1; // replace with the actual DoctorID
+$ClinicID = 1;
 // Assign a test value to AppointmentDateTime
 $AppointmentDateTime = "2023-12-22 14:30:00";
 $TimeSlotID	= 1;
